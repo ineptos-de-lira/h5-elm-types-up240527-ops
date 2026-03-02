be16815c40c87d4234af395cee19ddef5f1818a0
Moi <up240527@alumnos.upa.edu.mx>
2026-03-01 23:34:34 -0600

    subo la tarea

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,52 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+gradeValue : Float -> GradeStatus
+gradeValue grade =
+    if grade < 0 then
+        Pending
+
+    else if grade < 7 then
+        Failed
+
+    else
+        Approved
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map gradeValue grades
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction list =
+    List.map airplaneScheduleAction list
